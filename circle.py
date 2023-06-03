f7c6d22a896c3328e3b9b38f4abd15225f223bd9
Prithul Baveja <prithulbaveja@gmail.com>
2023-06-03 09:31:56 -0400

    circle problem

---
+++ circle.py
@@ -0,0 +1,17 @@
+nums = []
+for x in range(int(input())):
+    nums.append(int(input()))
+
+best_list = []
+for x in range(len(nums)):
+    
+    #for y in range(x):
+    thing = nums[0]
+    nums.remove(nums[0])
+    nums.insert(len(nums), thing)
+    curr=0
+    for v in range(len(nums)):
+        curr += v*nums[v]
+    best_list.append(curr)
+    
+print(min(best_list))
